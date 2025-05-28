dd27c2722fa2733acb7c40bc3fb5fc2069d6d8ef
generrc <ajohnston.professional@gmail.com>
2025-05-28 13:04:58 -0400

    testnoimage

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
 from flask import Flask, request, render_template
 import sqlite3
 
+DB_FILE = 'weather_database.db'
 
-con = sqlite3.connect("weather_database.db")
+
+def init_db():
+    cn = sqlite3.connect(DB_FILE)
+    c = cn.cursor()
+    c.execute()
+    cn.commit()
+    cn.close()
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    cn = sqlite3.connect(DB_FILE)
+    c = cn.cursor()
+    c.execute()
+    ccn.commit()
+    cn.close()
+    return {'status': 'ok'}
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute("SELECT datetime, temperature, air_pressure, humidity FROM weather ORDER BY id DESC LIMIT 10")
+    rows = c.fetchall()
+    conn.close()
+    return render_template('index.html', rows=rows)
